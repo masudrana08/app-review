7f791abab3af2748998e8d9f0468a0d0df463bf6
masudrana08 <rana028511@gmail.com>
2020-11-20 16:46:29 +0600

    filter design done

--- src/components/LeftBar/LeftBar.js
+++ src/components/LeftBar/LeftBar.js
@@ -4,8 +4,11 @@ import './LeftBar.css'
 const jsonData = require("../../review.json")
 const LeftBar = () => {
    const [store,setStore] =useContext(ReviewContext)
-
    let fiveStar=0, fourStar=0, threeStar=0, twoStar=0, oneStar=0;
+   const versions = []
+   const countries =[]
+
+  
    jsonData.forEach(data=>{
       if(store.appName.toLowerCase() == data.appID.substring(4)){
          if(data.rating==5){
@@ -20,9 +23,14 @@ const LeftBar = () => {
             oneStar++
          }
       }
+
+      if(!versions.includes(data.version)){
+         versions.push(data.version)
+      }
+      if(!countries.includes(data.countryName)){
+         countries.push(data.countryName)
+      }
    })
-
-
    return (
       <div >
          
@@ -36,8 +44,9 @@ const LeftBar = () => {
                   class="fa fa-minus-square minus-button"></i>
             </div>
 
+      {/* start rating filter */}
             <div>
-               <p>Filter by Rating</p>
+               <p className="filter-title">Filter by Rating</p>
                <div style={{display:"flex"}}>
                   <div>
                      <span className="fa fa-star checked"></span>
@@ -92,7 +101,24 @@ const LeftBar = () => {
                   </div>
                   <p className="star-count">{oneStar}</p>
                </div>
-               
+            </div>
+
+            <div>
+               <p className="filter-title">Filter by version</p>
+               {
+                  versions.map((data, index)=> {
+                     return <p className="small-p" key={index+"data"}>{data}</p>
+                  })
+               }
+            </div>
+
+            <div>
+               <p className="filter-title">Filter by country name</p>
+               {
+                  countries.map((data, index)=> {
+                     return <p className="small-p" key={index+"data"}>{data}</p>
+                  })
+               }
             </div>
          </div>
 
